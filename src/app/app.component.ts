8c79120170e4c69a75cdc9221af9528b12b39443
alvaro219 <alvaroxam72@gmail.com>
2023-12-26 18:43:26 +0100

    Gui-Grid implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
 import { ElectricChargingStation, ElectricChargingStationResponse } from './types';
 import { firstValueFrom } from 'rxjs';
+import { GuiColumn } from '@generic-ui/ngx-grid';
 
 @Component({
   selector: 'app-root',
@@ -32,59 +33,36 @@ export class AppComponent implements OnInit {
     firstValueFrom(this.dataService.getAutobahnen()).then(res => {
       this.autobahnen = res.roads;
     });
-    // this.dataService.getAutobahnen().then(roads => {
-    //   this.autobahnen = roads;
-    //   if (this.autobahnen.length > 0) {
-    //     const roadId = this.autobahnen[0];
-
-    //     this.dataService.getRoadWorks(roadId).then(works => {
-    //       this.roadworks = works.roadworks;
-
-    //       /* this.dataService.getElectricChargingStationDetails(stationId).then(stationDetails => {
-    //         this.electricChargingStationDetails = stationDetails; */
-
-    //       const roadworkId = works.roadworks[0].identifier;
-    //       const webcamId = 'V0VCQ0FNX19OUldfU2lsYS1TaWduYWxiYXVfMTAxMDgxMDk4ODE2NDgyOTQ4NTQ=';
-    //       const lorryId = 'UEFSS0lOR19fbWRtLmxvcnJ5LnBhcmtpbmdfX0RFLVNILTAwMTEwOA==';
-    //       const warningId = 'V0FSTklOR19fbWRtLnZpel9fTE1TLU5XL3JfTE1TLU5XLzMyNDE3OV9EICBOVyBMTVMtTlcuMA==';
-    //       const closureId = 'Q0xPU1VSRV9fbWRtLnZpel9fTE1TLU5XL3JfTE1TLU5XLzMxNTEyMV9EICBOVyBMTVMtTlcuMA==';
-    //       const stationId = 'RUxFQ1RSSUNfQ0hBUkdJTkdfU1RBVElPTl9fMTI2OTk=';
-
-    //       Promise.all([
-    //         this.dataService.getRoadWorkDetails(roadworkId),
-    //         this.dataService.getWebcamDetails(webcamId),
-    //         this.dataService.getLorryParkingDetails(lorryId),
-    //         this.dataService.getTrafficWarningDetails(warningId),
-    //         this.dataService.getClosureDetails(closureId),
-    //         this.dataService.getElectricChargingStationDetails(stationId)
-    //       ]).then(([roadWorkDetails, webcamDetails, lorryParkingDetails, trafficWarningDetails, closureDetails, electricChargingStationDetails]) => {
-    //         console.log('hola');
-    //         this.roadWorkDetails = roadWorkDetails;
-    //         this.webcamDetails = webcamDetails;
-    //         this.lorryParkingDetails = lorryParkingDetails;
-    //         this.trafficWarningDetails = trafficWarningDetails;
-    //         this.closureDetails = closureDetails;
-    //         this.electricChargingStationDetails = electricChargingStationDetails;
-    //       }).catch(error => {
-    //         console.error('Error fetching item details:', error);
-    //       });
-
-    //       // Obtener estaciones de carga y detalles específicos de una estación
-    //       this.dataService.getElectricChargingStations(roadId).then(stations => {
-    //         this.electricChargingStations = stations;
-
-    //         const stationId = 'RUxFQ1RSSUNfQ0hBUkdJTkdfU1RBVElPTl9fMTI2OTk=';
-
-    //         this.dataService.getElectricChargingStationDetails(stationId).then(stationDetails => {
-    //           this.electricChargingStationDetails = stationDetails;
-    //         }).catch(error => {
-    //           console.error('Error fetching station details:', error);
-    //         });
-    //       }).catch(error => {
-    //         console.error('Error fetching charging stations:', error);
-    //       });
-    //     });
-    //   }
-    // });
   }
+  source: Array<any> = [
+    {
+      name: 'Brad',
+      job: 'programmer',
+      age: '40'
+    },
+    {
+      name: 'John',
+      job: 'athlete',
+      age: '22'
+    },
+    {
+      name: 'Eve',
+      job: 'artist',
+      age: '25'
+    }
+];
+columns: Array<GuiColumn> = [
+  {
+    header: 'Name',
+    field: 'name'
+  },
+  {
+    header: 'Job',
+    field: 'job'
+  },
+  {
+    header: 'Age',
+    field: 'age'
+  }
+];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { GuiGridModule } from '@generic-ui/ngx-grid';
+
 import { AppComponent } from './app.component';
 import { RoadworksComponent } from './components/roadworks/roadworks.component';
 import { ParkingLorryComponent } from './components/parking_lorry/parking_lorry.component';
@@ -12,7 +14,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MaterialModule } from './material.module';
 import { WarningComponent } from './components/warning/warning.component';
 import { ClosureComponent } from './components/closure/closure.component';
-import { ChargingStationComponent } from './components/charging_Station/charging_station.component';
+import { ChargingStationComponent } from './components/charging_station/charging_station.component';
 
 
 @NgModule({
@@ -31,6 +33,7 @@ import { ChargingStationComponent } from './components/charging_Station/charging
     BrowserAnimationsModule,
     RouterModule,
     MaterialModule,
+    GuiGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
